86babc4d386b8e55b269e29ca4b61913a64bd345
Olivier St-Amand <olivier.st.amand.1@gmail.com>
2021-12-28 19:51:44 -0500

    Add notifications support (#9)

---
+++ web/notify/notify.go
@@ -0,0 +1,80 @@
+package notify
+
+import (
+	"fmt"
+	"net/http"
+	"strconv"
+)
+
+type Notification int
+type Status int
+
+const (
+	NotifyNone          Notification = 0
+	NotifySignedIn      Notification = 101
+	NotifyLinkCreated                = 102
+	NotifyWrongPassword              = 301
+)
+
+const (
+	StatusNone    Status = 0
+	StatusInfo    Status = 1
+	StatusSuccess        = 2
+	StatusWarning        = 3
+	StatusError          = 4
+)
+
+var AllNotifications = []Notification{
+	NotifyNone, NotifySignedIn,
+	NotifyLinkCreated, NotifyWrongPassword,
+}
+
+var notificationToText = map[Notification]string{
+	NotifyNone:          "",
+	NotifySignedIn:      "Welcome! You successfully signed in",
+	NotifyLinkCreated:   "New link created successfully",
+	NotifyWrongPassword: "Your username or password is incorrect. Try again",
+}
+
+var intToNotification map[int]Notification
+
+func init() {
+	intToNotification = make(map[int]Notification)
+	for _, n := range AllNotifications {
+		intToNotification[int(n)] = n
+	}
+}
+
+func (n Notification) String() string {
+	return notificationToText[n]
+}
+
+func (n Notification) Status() Status {
+	switch {
+	case n > 300:
+		return StatusError
+	case n > 200:
+		return StatusWarning
+	case n > 100:
+		return StatusSuccess
+	case n > 0:
+		return StatusInfo
+	default:
+		return StatusNone
+	}
+}
+
+func GetNotification(req *http.Request) Notification {
+	keys, ok := req.URL.Query()["n"]
+	if !ok || len(keys) < 1 {
+		return NotifyNone
+	}
+	if id, err := strconv.Atoi(keys[0]); err == nil {
+		return intToNotification[id]
+	}
+	return NotifyNone
+}
+
+func AddNotificationToURL(url string, notification Notification) string {
+	return fmt.Sprintf("%s?n=%d", url, int(notification))
+}
--- web/view_model.go
+++ web/view_model.go
@@ -1,21 +1,47 @@
 package main
 
-import "github.com/ostamand/url/web/store"
+import (
+	"net/http"
+
+	"github.com/ostamand/url/web/notify"
+	"github.com/ostamand/url/web/store"
+)
 
 const SessionCookie = "session_token"
 
-func CreateViewModel(user *store.UserModel) *ViewModel {
-	loggedIn := false
-	if user.ID != 0 {
-		loggedIn = true
+func CreateViewModel(req *http.Request, u *store.UserModel) *ViewModel {
+	data := ViewModel{User: u}
+
+	// flag for user login
+	if u != nil && u.ID != 0 {
+		data.LoggedIn = true
 	}
-	return &ViewModel{
-		LoggedIn: loggedIn,
-		User:     user,
+
+	// process notifications from url
+	n := notify.GetNotification(req)
+	if n != notify.NotifyNone {
+		switch n.Status() {
+		case notify.StatusInfo:
+			data.StatusInfo = true
+		case notify.StatusSuccess:
+			data.StatusSuccess = true
+		case notify.StatusWarning:
+			data.StatusWarning = true
+		case notify.StatusError:
+			data.StatusError = true
+		}
+		data.StatusText = n.String()
 	}
+
+	return &data
 }
 
 type ViewModel struct {
-	LoggedIn bool
-	User     *store.UserModel
+	LoggedIn      bool
+	User          *store.UserModel
+	StatusText    string
+	StatusInfo    bool
+	StatusSuccess bool
+	StatusWarning bool
+	StatusError   bool
 }
