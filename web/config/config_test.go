94541319c1742db925896198a164c70877a0736c
O1SA <olivier.st.amand.1@gmail.com>
2021-12-30 19:55:04 -0500

    Fix test

--- web/config/config_test.go
+++ web/config/config_test.go
@@ -12,7 +12,7 @@ const configPath = "config_dev.json"
 
 func TestCanGetConfig(t *testing.T) {
 	wd, _ := os.Getwd()
-	fullPath := filepath.Join(filepath.Dir(wd), configPath)
+	fullPath := filepath.Join(filepath.Dir(filepath.Dir(wd)), configPath)
 	data := Get(fullPath)
 	assert.Equal(t, "shorturl", data.Db.Name)
 	assert.Equal(t, "root", data.Db.User)
